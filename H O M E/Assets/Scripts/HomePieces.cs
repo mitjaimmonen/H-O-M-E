1f6c582c8baa5c55ed602123d665b9d4f70a271f
Antonio <antonio.omt@gmail.com>
2019-01-25 22:50:35 +0200

    Game Master

---
+++ H O M E/Assets/Scripts/HomePieces.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomePieces : MonoBehaviour
+{
+    public bool Occupied;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DriftSlowly()
+    {
+        //random direction drift slowly
+    }
+
+    void Spawn()
+    {
+
+    }
+
+
+}
---
+++ H O M E/Assets/Scripts/Managers/GameMaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    Camera mainCamera;
+    HoleSpawner HoleSpawner;
+    List<HomePieces> HomePieces = new List<HomePieces>();
+
+    private static GameMaster _instance;
+    public static GameMaster Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
