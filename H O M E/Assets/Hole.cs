094c0c0cea4ab38a46906a4a12d0197dc0e21a1d
Unknown <mitja.immonen@hotmail.com>
2019-01-25 22:55:14 +0200

    Attractive hole

---
+++ H O M E/Assets/Hole.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public Shape shape = Shape.round;
+    public float attractDistance = 1f;
+    public float snapDistance = 0.25f;
+    
+    void OnTriggerStay2D(Collider2D col)
+    {
+        float magnitude = ((Vector2)col.transform.position - (Vector2)transform.position).magnitude;
+        if (magnitude < snapDistance)
+        {
+            if (col.GetComponent<Player>())
+            {
+                col.GetComponent<Player>().SnugFit(this);
+            }
+        }
+        else if (magnitude < attractDistance)
+        {
+            if (col.GetComponent<Player>())
+            {
+                col.GetComponent<Player>().Attract(this, magnitude/attractDistance);
+            }
+        }
+    }
+}
--- H O M E/Assets/Scripts/Player.cs
+++ H O M E/Assets/Scripts/Player.cs
@@ -2,30 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Shape
+{
+    round,
+    triangle,
+    square,
+    pentagon,
+    hexagon
+}
+
 public class Player : MonoBehaviour
 {
+    public Shape shape = Shape.round;
+    public float speed = 5f;
+    public float acceleration = 5f;
+    public float deacceleration = 2f;
+    public float attractForce = 5f;
 
-    public float speed;
-    public float acceleration;
-    public float deacceleration;
-
+    private bool allowControl = true;
     private Vector2 velModifier;
-    private Vector2 velocity;
-
+    private bool moveInput;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (allowControl)
+            HandleInput();
+    }
+
+    void FixedUpdate()
+    {
+        ApplyVelocity();
+    }
+
+    void HandleInput()
+    {
+        moveInput = false;
         if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
         {
             velModifier.x = Input.GetAxis("Horizontal");
-
+            moveInput = true;
         }
         else
         {
@@ -34,10 +57,47 @@ public class Player : MonoBehaviour
         if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f)
         {
             velModifier.y = Input.GetAxis("Vertical");
+            moveInput = true;
         }
         else
         {
             velModifier.y = 0;
         }
     }
+
+    void ApplyVelocity()
+    {
+        rb.AddForce(-rb.velocity * deacceleration);
+
+        if (moveInput)
+        {
+            rb.AddForce(velModifier * acceleration);
+        }
+
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, speed);
+    }
+
+    public void SnugFit(Hole hole)
+    {
+        if (hole.shape == shape)
+        {
+            transform.position = new Vector3(hole.transform.position.x, hole.transform.position.y, transform.position.z);
+            velModifier = Vector2.zero;
+            rb.velocity = Vector2.zero;
+            rb.isKinematic = true;
+            allowControl = false;
+        }
+    }
+
+    public void Attract(Hole hole, float distance01)
+    {
+        Vector2 dir = (Vector2)hole.transform.position - (Vector2)transform.position;
+        dir.Normalize();
+
+        if (hole.shape == shape)
+        {
+            rb.AddForce(dir*attractForce * Time.deltaTime * (1f-distance01));
+        }
+    }
+
 }
