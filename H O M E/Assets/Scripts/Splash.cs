e2c3a3fd05e678df777d05ca77c5ffb1a9909ed3
Unknown <mitja.immonen@hotmail.com>
2019-01-27 04:14:47 +0200

    splash & menu transition

---
+++ H O M E/Assets/Scripts/Splash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneName;
+    public AudioSource introSound;
+    public AudioSource introSound2;
+
+
+    AsyncOperation async;
+    void Start()
+    {
+        StartCoroutine(LoadLoop());
+    }
+    
+    IEnumerator LoadLoop() {
+        async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+        yield return async;
+     }
+ 
+    public void ActivateScene() {
+        async.allowSceneActivation = true;
+    }
+
+    public void PlayIntroSound()
+    {
+        introSound.Play();
+    }
+    public void PlayIntroSound2()
+    {
+        introSound2.Play();
+    }
+}
